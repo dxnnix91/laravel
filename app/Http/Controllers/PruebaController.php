4c78845b79ee9f4fb053b7bfb917dd566d963943
Daniel <dxnnix91@gmail.com>
2021-07-15 21:17:44 -0500

    Primer commit

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+    function prueba(){
+        return 'Hola desde prueba controller';
+    }
+}
